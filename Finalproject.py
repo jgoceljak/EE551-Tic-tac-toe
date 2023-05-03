b2affcac81ebad9ee8844e4923bd3371f2deac83
John Goceljak <43759565+jgoceljak@users.noreply.github.com>
2023-05-03 19:53:05 -0400

    working 5x5

--- Finalproject.py
+++ Finalproject.py
@@ -80,8 +80,9 @@ def initializeBig():
 def displayBoard(boardsize):
     
     print("GAME BOARD\n")
-    for x in range(3):
-        print(board[x][0] + "  " + board[x][1] + "  " + board[x][2]) #Shows Board by displaying each element
+    for x in range(boardsize):
+        for y in range(boardsize):
+            print(board[x][y] + "  ", end='') #Shows Board by displaying each element
         print("\n")
   
 def checkWinner():
@@ -124,11 +125,11 @@ def checkWinner():
 def checkWinnerBig():
 
     for x in range(5): #check verticals
-        if board[0][x] == 'X' and board[1][x] == 'X' and board[2][x] == 'X' and board[3][x] == 'X' and board[4][x] == 'X'
+        if board[0][x] == 'X' and board[1][x] == 'X' and board[2][x] == 'X' and board[3][x] == 'X' and board[4][x] == 'X':
             winner = 1
             isWinner = True
             return winner
-        elif board[0][x] == 'O' and board[1][x] == 'O' and board[2][x] == 'O' and board[3][x] == 'O' and board[4][x] == 'O'
+        elif board[0][x] == 'O' and board[1][x] == 'O' and board[2][x] == 'O' and board[3][x] == 'O' and board[4][x] == 'O':
             winner = 2
             isWinner = True
             return winner
@@ -176,7 +177,7 @@ def game():
         winner = checkWinner()
         if winner == 1 or winner == 2:
             isWinner == True
-            displayBoard()
+            displayBoard(3)
             print("Player " + str(winner) + " has won!\n")
             if winner == 1:
                 player1Score += 1
@@ -195,8 +196,8 @@ def game():
             print("Player 1 Go\n")
         else:
             print("Player 2 Go\n")
-        displayBoard()    
-        filled = checkFilled()
+        displayBoard(3)    
+        filled = checkFilled(3)
         if filled:                                          #prompts to reset after a draw
             gameOver = input("DRAW! Press 0 to restart the game, or 1 to reset everything\n")
             while gameOver != "0" and gameOver != "1":
@@ -235,10 +236,10 @@ def gameBig():
     validSpace = False
     while isWinner == False: #Loop Game alternating players until there is a winner
         validSpace = False
-        winner = checkWinner()
+        winner = checkWinnerBig()
         if winner == 1 or winner == 2:
             isWinner == True
-            displayBoard()
+            displayBoard(5)
             print("Player " + str(winner) + " has won!\n")
             if winner == 1:
                 player1Score += 1
@@ -257,25 +258,25 @@ def gameBig():
             print("Player 1 Go\n")
         else:
             print("Player 2 Go\n")
-        displayBoard()    
-        filled = checkFilled()
+        displayBoard(5)    
+        filled = checkFilled(5)
         if filled:                                          #prompts to reset after a draw
             gameOver = input("DRAW! Press 0 to restart the game, or 1 to reset everything\n")
             while gameOver != "0" and gameOver != "1":
                 gameOver =  input("Press 0 to restart the game, or 1 to reset everything\n")
             if gameOver == "0":
                 print("Resetting Game Board\n")
-                initialize()
+                initializeBig()
             elif gameOver == "1":
                 print("Resetting Everything\n")
                 main()
         while validSpace == False:                         #checks if what the player selects is a valid space
-            row = input("Player " + str(currentTurn) + ", choose a row from 1 to 3\n")
-            while row != "1" and row != "2" and row != "3":
-                row = input("Invalid input, input either 1, 2, or 3\n")
-            column = input("Player " + str(currentTurn) + ", now choose a column from 1 to 3\n")
-            while column != "1" and column != "2" and column != "3":
-                column = input("Invalid input, input either 1, 2, or 3\n")
+            row = input("Player " + str(currentTurn) + ", choose a row from 1 to 5\n")
+            while row != "1" and row != "2" and row != "3" and row != "4" and row != "5":
+                row = input("Invalid input, input either 1, 2, 3, 4, or 5\n")
+            column = input("Player " + str(currentTurn) + ", now choose a column from 1 to 5\n")
+            while column != "1" and column != "2" and column != "3" and column != "4" and column != "5":
+                column = input("Invalid input, input either 1, 2, 3, 4, or 5\n")
             if board[int(row)-1][int(column)-1] != "□":
                 print("That position is already occupied!")
             else:
